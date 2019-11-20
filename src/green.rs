182ac2c12e747eafcc664d2decdd1e64a462b8dd
cad97 <cad97@cad97.com>
2019-11-20 14:37:05 -0500

    nuke

--- src/green.rs
+++
@@ -1,7 +0,0 @@
-mod node;
-mod token;
-mod element;
-mod builder;
-
-pub(crate) use self::element::*;
-pub use self::{builder::*, node::*, token::*};
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,10 @@
 //! A generic library for lossless syntax trees.
-//! See `examples/s_expressions.rs` for a tutorial.
-#![forbid(
-    // missing_debug_implementations,
-    unconditional_recursion,
-    future_incompatible,
-    // missing_docs,
-)]
+
+#![forbid(unconditional_recursion, future_incompatible)]
+#![warn(missing_debug_implementations, missing_docs)]
 #![deny(unsafe_code)]
 
-mod green;
-#[allow(unsafe_code)]
-pub mod cursor;
-pub mod api;
-mod syntax_text;
-mod utility_types;
-#[cfg(feature = "serde1")]
-mod serde_impls;
+#[cfg(feature = "serde")]
+extern crate serde_ as serde; // rename back
 
-#[allow(unused)]
-use green::{GreenElement, GreenElementRef};
-
-// Reexport types for working with strings. We might be too opinionated about
-// these, as a custom interner might work better, but `SmolStr` is a pretty good
-// default.
-pub use smol_str::SmolStr;
 pub use text_unit::{TextRange, TextUnit};
-
-pub use crate::{
-    api::*,
-    green::{Checkpoint, GreenNode, GreenNodeBuilder, GreenToken, Children},
-    syntax_text::SyntaxText,
-    utility_types::{Direction, NodeOrToken, TokenAtOffset, WalkEvent},
-};
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn assert_send_sync() {
-        fn f<T: Send + Sync>() {}
-        f::<GreenNode>();
-    }
-
-    #[test]
-    fn test_size_of() {
-        use std::mem::size_of;
-
-        eprintln!("GreenNode    {}", size_of::<GreenNode>());
-        eprintln!("GreenToken   {}", size_of::<GreenToken>());
-        eprintln!("GreenElement {}", size_of::<GreenElement>());
-        eprintln!();
-        eprintln!("SyntaxNode    {}", size_of::<cursor::SyntaxNode>());
-        eprintln!("SyntaxToken   {}", size_of::<cursor::SyntaxToken>());
-        eprintln!("SyntaxElement {}", size_of::<cursor::SyntaxElement>());
-    }
-}
